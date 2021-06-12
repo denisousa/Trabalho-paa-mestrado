f7950f6c8cb1ee64f34aabaed05145edbb012502
Denis Sousa <denis.sousa@aluno.uece.br>
2021-06-12 09:39:50 -0300

    finish

--- operations.py
+++ operations.py
@@ -6,5 +6,5 @@ def assignment(literals, clauses):
         l3 = literals[abs(clause[2])-1] if clause[2] > 0 else not literals[abs(clause[2])-1]
         r = l1 and l2 and l3 
         if r: 
-            true_clauses+=1
+            true_clauses += 1
     return true_clauses
--- paa.py
+++ paa.py
@@ -1,20 +1,29 @@
-# Conseguir as devidas atribuições, que gerem o máixmo de cláusulas verdadeiras
-# (x ^ ~y ^ z) || (~x ^ y ^ z)
-from operations import assignment
+def assignment(literals, clauses):
+    true_clauses = 0
+    for clause in clauses:
+        l1 = literals[abs(clause[0])-1] if clause[0] > 0 else not literals[abs(clause[0])-1]
+        l2 = literals[abs(clause[1])-1] if clause[1] > 0 else not literals[abs(clause[1])-1]
+        l3 = literals[abs(clause[2])-1] if clause[2] > 0 else not literals[abs(clause[2])-1]
+        r = l1 and l2 and l3 
+        if r: 
+            true_clauses += 1
+    return true_clauses
 
-sat_files = ['SAT1.txt', 'SAT2.txt', 'SAT3.txt']
-for sat in sat_files:
-    dataset = open(sat).readlines()
-    range_literal, range_clause = list(map(int, dataset[0].replace(' \n', '').split(' ')))
-    clauses = []
+def main():
+    sat_files = ['SAT1.txt', 'SAT2.txt', 'SAT3.txt']
+    for sat in sat_files:
+        dataset = open(sat).readlines()
+        range_literal, range_clause = list(map(int, dataset[0].replace(' \n', '').split(' ')))
+        clauses = []
 
-    for line in dataset[1:range_clause+1]:
-        clauses.append(list(map(int, line.replace(' \n', '').split(' '))))
+        for line in dataset[1:range_clause+1]:
+            clauses.append(list(map(int, line.replace(' \n', '').split(' '))))
 
-    literals_false = [False for i in range(range_clause)]
-    literals_input = [True if num == '1' else False for num in dataset[-1].replace('\n', '')]
+        literals_false = [False for i in range(range_clause)]
+        literals_input = [True if num == '1' else False for num in dataset[-1].replace('\n', '')]
 
-    print(sat)
-    print(f'literals 0: {assignment(literals_false, clauses)}')
-    print(f'literals input: {assignment(literals_input, clauses)} \n')
+        print(sat)
+        print(f'literals 0: {assignment(literals_false, clauses)}')
+        print(f'literals input: {assignment(literals_input, clauses)} \n')
 
+main()
