c9a3a51a95505572322f77bd7db373bad21296a8
adrilene <adrilenefonseca@gmail.com>
2021-06-30 15:09:01 -0300

    progress

--- work/tabu_search.py
+++ work/tabu_search.py
@@ -10,12 +10,12 @@ def generate_neighborhood(solution):
     global nv
     neighborhood = []
     while len(neighborhood) < nv:
-        neighbor = copy.deepcopy(solution)
+        neighbor = copy.deepcopy(solution[0])
         random.seed(time.time())
         index1, index2 = random.sample(range(0, len(solution)), k=2)
         neighbor[index1] = not neighbor[index1]
         neighbor[index2] = not neighbor[index2]
-        if neighbor not in neighborhood:
+        if not [True for i in neighborhood if i[0] == neighbor]:
             neighborhood.append((neighbor, index1, index2))
 
     return neighborhood
@@ -45,29 +45,52 @@ def assignment(literals, clauses):
     return true_clauses
 
 
-def aspiration(current_value):
-    pass
+def aspiration(best_solution, best_value, current_value):
+    if best_value > current_value:
+        return True
+    return False
 
 
 def update_tabu(queue, value):
     global tabu_size
     if len(queue) >= tabu_size:
         queue.pop(0)
-    return queue.append(value)
+    queue.append(sorted(value))
+    return queue
 
 
 def tabu_search(initial_solution, clauses):
     current_solution = copy.deepcopy(initial_solution)
-    current_value = assignment(current_solution, clauses)
+    current_value = assignment(current_solution[0], clauses)
     tabu_queue = []
+    count = 0
 
-    neighborhood = generate_neighborhood(current_solution)
-    for neighbor in neighborhood:
-        neighbor_value = assignment(neighbor[0], clauses)
-        if neighbor_value > current_value:
-            current_solution = neighbor[0]
-            current_value = neighbor_value
-            tabu_queue = update_tabu(tabu_queue, neighbor[1:3])
+    while count < 5:
+        neighborhood = generate_neighborhood(current_solution)
+        neighborhood_values = [
+            assignment(neighborhood[i][0], clauses) for i in range(len(neighborhood))
+        ]
+        max_index = neighborhood_values.index(max(neighborhood_values))
+        best_solution = neighborhood[max_index]
+
+        while True:
+            # check if all elements have been visited
+            if sorted(best_solution[1:3]) in tabu_queue:
+                if aspiration(
+                    best_solution, neighborhood_values[max_index], current_value
+                ):
+                    current_solution = best_solution
+                    break
+                else:
+                    neighborhood_values[max_index] = 0
+                    max_index = neighborhood_values.index(max(neighborhood_values))
+                    best_solution = neighborhood[max_index]
+            else:
+                current_solution = best_solution
+                current_value = neighborhood_values[max_index]
+                tabu_queue = update_tabu(tabu_queue, current_solution[1:3])
+                break
+        count += 1
 
     return current_solution, current_value
 
@@ -84,7 +107,7 @@ def main():
     for line in dataset[1 : range_clause + 1]:
         clauses.append(list(map(int, line.replace(" \n", "").split(" "))))
 
-    initial_solution = [False for i in range(range_literal)]
+    initial_solution = ([False for i in range(range_literal)], -1, -1)
     print(tabu_search(initial_solution, clauses))
 
 
